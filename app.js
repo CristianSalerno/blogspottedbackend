6953eda218a77ec09faa3898472e815175965284
Cristian Salerno <cristiansalerno18@gmail.com>
2021-01-07 22:20:03 +0100

    conection to mongo cluster, added .env

--- app.js
+++ app.js
@@ -2,29 +2,19 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-
-const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
+require('dotenv').config();
 
-const MONGO_URI = 'mongodb+srv://csalerno:Tecomiste7@blogspotted.mkux9.mongodb.net/<dbname>?retryWrites=true&w=majority';
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-const mongo_uri = ''
-
-app.use(express.static(path.join(__dirname, '../blogspotted')));
-
-app.get('/', (req, res) => {
-
-})
+app.use(express.static(path.join(__dirname, process.env.FRONTEND_URI)));
 
 app.listen(3000, () => {
     console.log('server started')
-})
+});
 
 module.exports = app;
