06be506eb338167afdd2036f24d7e4e0147048ed
Cristian Salerno <cristiansalerno18@gmail.com>
2021-01-07 21:59:43 +0100

    mongodb cluster uri added

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const app = express();
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
+
+const MONGO_URI = 'mongodb+srv://csalerno:Tecomiste7@blogspotted.mkux9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
 const mongo_uri = ''
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../blogspotted')));
 
 app.get('/', (req, res) => {
 
 })
+
 app.listen(3000, () => {
     console.log('server started')
 })
